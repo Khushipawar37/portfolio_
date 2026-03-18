0c6b756fc08e152089f46bc1083818cb5c358c31
Khushipawar37 <khushipawar987@gmail.com>
2026-03-18 20:31:44 +0530

    Removed ts errors

--- src/hooks/useScrollAnimation.ts
+++ src/hooks/useScrollAnimation.ts
@@ -2,10 +2,10 @@ import { useRef } from "react";
 import { useInView } from "framer-motion";
 
 export function useScrollAnimation(
-  options: { once?: boolean; margin?: string } = {}
+  options: { once?: boolean; margin?: any } = {}
 ) {
   const { once = true, margin = "-80px" } = options;
   const ref = useRef(null);
-  const inView = useInView(ref, { once, margin });
+  const inView = useInView(ref, { once, margin } as any);
   return { ref, inView };
 }
